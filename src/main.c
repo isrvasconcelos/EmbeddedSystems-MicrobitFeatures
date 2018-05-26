0aac6e35fb36bf75d6c6495fad302224eedbc193
Israel <vasoncelos.isr@gmail.com>
2018-05-26 09:00:10 -0300

    Accelerometer with thresholds

--- src/main.c
+++ src/main.c
@@ -23,6 +23,7 @@
 #define ACC_DEV_ADDR     0x1D
 #define ACC_WHO_AM_I_REG 0x0D
 #define ACC_TEST_VALUE   0x5A
+#define ACC_OUT_X_MSB 0x01
 
 #define COMPASS_DEV_ADDR     0x0e
 #define COMPASS_WHO_AM_I_REG 0x07
@@ -30,6 +31,11 @@
 
 struct i2c_dev acc, compass;
 u8_t data[2];
+bool compass_enabled = false;
+bool acc_enabled = false;
+bool temperature_enabled = false;
+
+#define RESET_ALL_SENSORS() ({compass_enabled = false; acc_enabled = false; temperature_enabled = false;})
 
 /***************************************************************************************/
 /** STATE MACHINE **/
@@ -61,34 +67,37 @@ static struct sensor_value temp_value;
 static uint8_t current_temperature;
 
 void s1_display() { /* Show some scrolling text ("ECOM042.2017.2") */
+	printk("1: Display text.\n");
+
 	struct mb_display *disp = mb_display_get();
 	mb_display_print(disp, MB_DISPLAY_MODE_DEFAULT | MB_DISPLAY_FLAG_LOOP, 
 		K_MSEC(500), "ECOM042.2017.2");
 }
 
 void s2_accelerometer() {
+	printk("2: Accelerometer enabled.\n");
+	acc_enabled=true;
 
 }
 
 void s3_compass() {
-	struct mb_display *disp = mb_display_get();
-	mb_display_print(disp, MB_DISPLAY_MODE_SINGLE,
-		K_SECONDS(1), "C");
+	printk("3: Compass enabled.\n");
+	compass_enabled=true;
 }
 
 void s4_temperature() {
 
-	struct mb_display *disp = mb_display_get();
+	//struct mb_display *disp = mb_display_get();
+	temperature_enabled=true;
 
-	printk("State 4\n");
-	printf("Temperature is %dC\n", current_temperature);
+	printk("4: Temperature enabled.\n");
 
-	char output[3];
-	snprintk(output, 3, "%02d", current_temperature);
-	mb_display_print(disp, MB_DISPLAY_MODE_SINGLE, K_SECONDS(1), output);
 }
 
 void s5_bluetooth() {
+
+	printk("State 5\n");
+
 	struct mb_display *disp = mb_display_get();
 	mb_display_print(disp, MB_DISPLAY_MODE_SINGLE,
 		K_SECONDS(1), "D");
@@ -120,6 +129,7 @@ static void button_pressed(struct device *dev, struct gpio_callback *cb, u32_t p
 		current_event = FORWARD;
 	}
 
+	RESET_ALL_SENSORS();
 	current_state = machine[current_state].events[current_event];
 	machine[current_state].action();
 }
@@ -159,18 +169,30 @@ void main(void)
         SYS_LOG_WRN("Firmware version: v%d.%d.%d",
                         VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD);
 
+	struct mb_display *disp = mb_display_get();
+	struct mb_image pixel = {};
+
         i2c_util_dev_init(&acc, ACC_DEV_ADDR, "ACC", ACC_WHO_AM_I_REG,
                                 ACC_TEST_VALUE);
 
-	printf("Thermometer Example! %s\n", CONFIG_ARCH);
+        i2c_util_dev_init(&compass, COMPASS_DEV_ADDR, "COMPASS",
+COMPASS_WHO_AM_I_REG, COMPASS_TEST_VALUE);
+
+//	printf("Thermometer Example! %s\n", CONFIG_ARCH);
 
 	temp_dev = device_get_binding("TEMP_0");
 	if (!temp_dev) {
 		printk("error: no temp device\n");
 	}
 
-	printf("temp device is %p, name is %s\n",
-	       temp_dev, temp_dev->config->name);
+//	printf("temp device is %p, name is %s\n",
+//	       temp_dev, temp_dev->config->name);
+
+	
+	uint8_t enable_reg = 0x01;
+	i2c_util_write_bytes(&acc, 0x2A, &enable_reg, sizeof(enable_reg));
+
+	i2c_util_write_bytes(&compass, 0x10, &enable_reg, sizeof(enable_reg));
 
 	while (1) {
 		int r;
@@ -189,9 +211,45 @@ void main(void)
 
 		current_temperature = sensor_value_to_double(&temp_value);
 
-		i2c_util_read_bytes(&acc, 0x02, data, sizeof(data));
-		SYS_LOG_DBG("ACC X: %d", (data[1] << 8) | data[0]);
 
-		k_sleep(2000);
+		if(compass_enabled) {
+			i2c_util_read_bytes(&compass, 0x01, data, sizeof(data));
+			printk("COMPASS: x%d, ", data[1]);
+
+			i2c_util_read_bytes(&compass, 0x03, data, sizeof(data));
+			printk(" y%d, ", data[1]);
+
+			i2c_util_read_bytes(&compass, 0x05, data, sizeof(data));
+			printk(" z%d\n", data[1]);
+		}
+
+		if(acc_enabled) {
+			i2c_util_read_bytes(&acc, ACC_OUT_X_MSB, data, 6);
+
+			if((data[2] > 170 && data[4] > 170)) {
+
+				printk("ACC: x%d, ", data[0]);
+				printk(" y%d, ", data[2]);
+				printk(" z%d\n", data[4]);			
+			}
+
+
+			pixel.row[0] = BIT(1);
+			mb_display_image(disp, MB_DISPLAY_MODE_SINGLE,
+					 K_MSEC(250), &pixel, 1);
+			k_sleep(K_MSEC(300));
+
+
+		}
+
+		if(temperature_enabled) {
+			printf("Temperature is %dC\n", current_temperature);
+
+			char output[4];
+			snprintk(output, 4, "%02dC", current_temperature);
+			mb_display_print(disp, MB_DISPLAY_MODE_SINGLE, K_SECONDS(0.5), output);
+		}
+
+		k_sleep(250);
 	}
 }
